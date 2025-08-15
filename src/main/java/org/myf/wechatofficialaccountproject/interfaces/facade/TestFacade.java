1c3af13120ddd83bd95176ed7777a688bb5c0160
minyunfei <619707630@qq.com>
2025-08-15 20:31:17 +0800

    1

--- src/main/java/org/myf/wechatofficialaccountproject/interfaces/facade/TestFacade.java
+++ src/main/java/org/myf/wechatofficialaccountproject/interfaces/facade/TestFacade.java
@@ -18,7 +18,7 @@ public class TestFacade {
 
     @GetMapping("/index")
     public String index() {
-        System.out.println("1234");
+        System.out.println("12341");
         testServer.teacher("12");
         System.out.println(testServer);;
         return testServer.toString();
